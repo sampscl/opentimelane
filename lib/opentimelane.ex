0d776a695c65363056d020b4dfebaada41040451
Clay Sampson <pdgeek@gmail.com>
2020-02-29 09:15:40 -0500

    first commit

---
+++ lib/opentimelane.ex
@@ -0,0 +1,18 @@
+defmodule Opentimelane do
+  @moduledoc """
+  Documentation for Opentimelane.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Opentimelane.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/opentimelane_test.exs
@@ -0,0 +1,8 @@
+defmodule OpentimelaneTest do
+  use ExUnit.Case
+  doctest Opentimelane
+
+  test "greets the world" do
+    assert Opentimelane.hello() == :world
+  end
+end
